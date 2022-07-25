92d00f0238698e6fc4afd422619d1373f2646e11
Plinioc <plinio.lsc@gmail.com>
2022-07-25 00:44:17 -0300

    Implement menu to run scripts

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-// const scrapFiis = require('./scripts/scrapFiis')
+const scrapFiis = require('./scripts/scrapFiis')
 const scrapAllFunds = require('./scripts/scrapAllFunds')
-scrapAllFunds.run()
-// scrapFiis.run()
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Select an action: \n 1 - Scrap Fund List \n 2 - Scrap a Single Fund \n 0 - Exit \n'
+});
+
+rl.prompt();
+
+rl.on('line', async (line) => {
+  if (line.toLowerCase() === "exit" || line == "0") {
+    console.log('\nExiting!\n');
+    process.exit(0);        
+  } else if (line.trim() === "1") {
+    await scrapAllFunds.run()
+  } else if (line.trim() === "2") {
+    await scrapFiis.run()
+  } else {
+    console.log(`\n No action \n`)
+  }
+  rl.prompt();
+}).on('close', () => {
+  console.log('Exiting!');
+  process.exit(0);
+});
