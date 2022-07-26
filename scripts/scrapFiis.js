a9433099f629729f316a8f8bca4bbf2fb0536907
Plinioc <plinio.lsc@gmail.com>
2022-07-26 01:10:37 -0300

    Implement read csv

--- scripts/scrapFiis.js
+++ scripts/scrapFiis.js
@@ -1,5 +1,54 @@
 const puppeteer = require('puppeteer')
 const exportCsv = require('../utils/exportCsv')
+const readCsv = require('../utils/readCsv')
+
+const navidateAndScrap = async (page, url) => {
+  // Site
+  console.log('Navigating to ', url)
+  await page.goto(url)
+
+  await page.waitForSelector('#fund-actives-items-wrapper .funds-data', {timeout: 2500})
+
+  const assetsLocation = await page.evaluate(() => {
+
+    const assets = []
+
+    document.querySelectorAll('#fund-actives-items-wrapper .items-wrapper:not(:first-child)').forEach((itemWrapper) => {
+      const title = itemWrapper.querySelector('.title').innerText
+      const listItems = [...itemWrapper.querySelectorAll('li')]
+      const address = listItems.find(li => li.innerText.includes('Endereço'))?.innerText.replace('Endereço: ', '')
+      const neighborhood = listItems.find(li => li.innerText.includes('Bairro'))?.innerText.replace('Bairro: ', '')
+      const city = listItems.find(li => li.innerText.includes('Cidade'))?.innerText.replace('Cidade: ', '')
+      const squareMeters = listItems.find(li => li.innerText.includes('Área Bruta'))?.innerText.replace('Área Bruta Locável: ', '')
+        
+      const assetData = {
+        title,
+        address,
+        neighborhood,
+        city,
+        squareMeters,
+      }
+
+      assets.push(assetData)
+    })
+
+    return assets
+  }, {})
+
+  exportCsv.export("./scrapped-content/assets-location.csv", assetsLocation.map(asset => {
+
+    const [city, state] = asset.city.split('-').map(i => i.trim())
+
+    return {
+      title: asset.title,
+      address: asset.address,
+      neighborhood: asset.neighborhood,
+      squareMeters: asset.squareMeters,
+      city,
+      state,
+    }
+  }))
+}
 
 module.exports = {
   async run() {
@@ -10,54 +59,11 @@ module.exports = {
       ]
     })
 
+    const test = await readCsv.readAll("./scrapped-content/assets-location.csv")
+
     const page = await browser.newPage()
 
-    // Site
-    const baseUrl = 'https://www.fundsexplorer.com.br/funds/rbrp11'
-    console.log('Navigating to ', baseUrl)
-    await page.goto(baseUrl)
-
-    await page.waitForSelector('#fund-actives-items-wrapper .funds-data')
-
-    const assetsLocation = await page.evaluate(() => {
-
-      const assets = []
-
-      document.querySelectorAll('#fund-actives-items-wrapper .items-wrapper:not(:first-child)').forEach((itemWrapper) => {
-        const title = itemWrapper.querySelector('.title').innerText
-        const listItems = [...itemWrapper.querySelectorAll('li')]
-        const address = listItems.find(li => li.innerText.includes('Endereço'))?.innerText.replace('Endereço: ', '')
-        const neighborhood = listItems.find(li => li.innerText.includes('Bairro'))?.innerText.replace('Bairro: ', '')
-        const city = listItems.find(li => li.innerText.includes('Cidade'))?.innerText.replace('Cidade: ', '')
-        const squareMeters = listItems.find(li => li.innerText.includes('Área Bruta'))?.innerText.replace('Área Bruta Locável: ', '')
-         
-        const assetData = {
-          title,
-          address,
-          neighborhood,
-          city,
-          squareMeters,
-        }
-  
-        assets.push(assetData)
-      })
-  
-      return assets
-    }, {})
-
-    exportCsv.export("./scrapped-content/assets-location.csv", assetsLocation.map(asset => {
-
-      const [city, state] = asset.city.split('-').map(i => i.trim())
-
-      return {
-        title: asset.title,
-        address: asset.address,
-        neighborhood: asset.neighborhood,
-        squareMeters: asset.squareMeters,
-        city,
-        state,
-      }
-    }))
+    await navidateAndScrap(page, 'https://www.fundsexplorer.com.br/funds/abcp11')
 
     await browser.close()
   }
