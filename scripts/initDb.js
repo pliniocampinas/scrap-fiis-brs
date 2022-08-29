9acf0e0578c6cf9856daa7cb12dce74560c97a69
Plinioc <plinio.lsc@gmail.com>
2022-08-28 23:34:25 -0300

    Create a table for assets scraps

--- scripts/initDb.js
+++ scripts/initDb.js
@@ -1,27 +1,57 @@
 const connectionPool = require('../db/connectionPool')
 
+const createScrappedFundsTable = async () => {
+  console.log('initializing scrapped_funds')
+  const sql = `
+    CREATE TABLE IF NOT EXISTS scrapped_funds (
+      created_on TIMESTAMP NOT NULL,
+      acronym VARCHAR(10) NOT NULL,
+      url TEXT,
+      long_name TEXT,
+      admin TEXT,
+      PRIMARY KEY (created_on, acronym)
+    );
+  `
+  try {
+    await connectionPool.query(sql)
+  } catch(err) {
+    console.log('initializing scrapped_funds failed')
+    console.log(err)
+    return
+  }
+  console.warn('Succeeded')
+}
+
+const createScrappedAssetsTable = async () => {
+  console.log('initializing scrapped_assets')
+  const sql = `
+    CREATE TABLE IF NOT EXISTS scrapped_assets (
+      sequential SERIAL,
+      fund_acronym VARCHAR(10) NOT NULL,
+      title TEXT,
+      address TEXT,
+      neighborhood TEXT,
+      city TEXT,
+      state TEXT,
+      square_meters TEXT,
+      created_on TIMESTAMP NOT NULL,
+      PRIMARY KEY (sequential, fund_acronym)
+    );
+  `
+  try {
+    await connectionPool.query(sql)
+  } catch(err) {
+    console.log('initializing scrapped_assets failed')
+    console.log(err)
+    return
+  }
+  console.warn('Succeeded')
+}
+
 module.exports = {
   async run() {
     console.log('initializing tables')
-
-    console.log('initializing scrapped_funds')
-    const sql = `
-      CREATE TABLE IF NOT EXISTS scrapped_funds (
-        created_on TIMESTAMP NOT NULL,
-        acronym VARCHAR(10) NOT NULL,
-        url TEXT,
-        long_name TEXT,
-        admin TEXT,
-        PRIMARY KEY (created_on, acronym)
-      );
-    `
-    try {
-      await connectionPool.query(sql)
-    } catch(err) {
-      console.log('initializing scrapped_funds failed')
-      console.log(err)
-      return
-    }
-    console.warn('Succeeded')
+    await createScrappedFundsTable()
+    await createScrappedAssetsTable()
   }
 }
