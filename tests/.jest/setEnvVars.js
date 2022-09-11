b9b21748e00d97355c9cd88a779a4c0ee0164e5f
Plinioc <plinio.lsc@gmail.com>
2022-09-11 19:07:56 -0300

    Test db env variables

--- tests/.jest/setEnvVars.js
+++ tests/.jest/setEnvVars.js
@@ -1,1 +1,6 @@
-process.env.PLACES_API_KEY = 'TEST_PLACES_API_KEY';
+process.env.PLACES_API_KEY = 'TEST_PLACES_API_KEY'
+process.env.DB_USER = 'app_user'
+process.env.DB_HOST = 'localhost'
+process.env.DB_NAME = 'fundstestdb'
+process.env.DB_PASSWORD = 'app_user123'
+process.env.DB_PORT = 5432
--- tests/env.test.js
+++ tests/env.test.js
@@ -2,4 +2,12 @@ describe('env variable ', () => {
   it('PLACES_API_KEY should be a mock string', () => {
     expect(process.env.PLACES_API_KEY).toBe('TEST_PLACES_API_KEY')
   })
+
+  it('From Test database should be', () => {
+    expect(process.env.DB_USER).toBe('app_user')
+    expect(process.env.DB_HOST).toBe('localhost')
+    expect(process.env.DB_NAME).toBe('fundstestdb')
+    expect(process.env.DB_PASSWORD).toBe('app_user123')
+    expect(process.env.DB_PORT).toBe('5432')
+  })
 })
