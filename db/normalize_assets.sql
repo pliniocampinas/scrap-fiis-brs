5907c4bb188dcabf9f012d5177c0fe3b6ff87b6b
Plinioc <plinio.lsc@gmail.com>
2022-09-17 00:56:12 -0300

    Add more normalizing commands

--- db/normalize_assets.sql
+++ db/normalize_assets.sql
@@ -21,3 +21,66 @@ where city = 'Salvador - BA';
 
 update scrapped_assets set neighborhood = 'Liberdade' 
 where neighborhood = 'São Paulo/SP'
+
+update scrapped_assets set city = 'Iracemápolis' 
+where city = 'Iracemópolis'
+
+update cities_coordinates set city_name = 'Santa Bárbara d''Oeste' 
+where city_name = 'Santa Bárbara d`Oeste'
+
+update scrapped_assets set city = 'Santa Bárbara d''Oeste' 
+where city = 'Santa Bárbara d’Oeste'
+
+update scrapped_assets set city = 'Embu' 
+where city = 'Embu das Artes'
+
+update scrapped_assets set city = 'Brasília' 
+where city = 'Brasilia'
+
+update scrapped_assets set city = 'Quaraí' 
+where city = 'Quarai'
+
+update scrapped_assets set city = 'Campos dos Goytacazes' 
+where city = 'Campos Goytacazes'
+
+update scrapped_assets set city = 'Rio de Janeiro' 
+where city = 'RIO DE JANEIRO'
+
+update scrapped_assets set city = 'Maringá' 
+where city = 'Maringa'
+
+update scrapped_assets set city = 'Parobé' 
+where city = 'Parobe'
+
+update scrapped_assets set city = 'Juiz de Fora' 
+where city = 'Juiz Fora'
+
+update scrapped_assets set city = 'Niterói' 
+where city = 'Niteroi'
+
+update scrapped_assets set city = 'Cascavel' 
+where city = 'Cascável'
+
+update scrapped_assets set city = 'Sabáudia' 
+where city = 'Saubádia'
+
+update scrapped_assets set city = 'São Luís' 
+where city = 'São Luis'
+
+update scrapped_assets set city = 'Carapicuíba' 
+where city = 'Carapicuiba'
+
+update scrapped_assets set city = 'São Bernardo do Campo' 
+where city = 'São Bernando do Campo'
+
+update scrapped_assets set city = 'São José do Xingu' 
+where city = 'São Jose do Xingú'
+
+update scrapped_assets set city = 'Peruíbe' 
+where city = 'Peruibe'
+
+update scrapped_assets set city = 'Duque de Caxias' 
+where city = 'Duque de Caixas'
+
+update scrapped_assets set city = 'Manaus' 
+where city = 'Manuas'
