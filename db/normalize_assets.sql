c7ec9e4b35052b9c263c20551f84d042e5d230c0
Plinioc <plinio.lsc@gmail.com>
2022-09-18 21:21:58 -0300

    Set cities for assets with cities on its on name

--- db/normalize_assets.sql
+++ db/normalize_assets.sql
@@ -20,70 +20,277 @@ update scrapped_assets set city = 'Salvador'
 where city = 'Salvador - BA';
 
 update scrapped_assets set neighborhood = 'Liberdade' 
-where neighborhood = 'São Paulo/SP'
+where neighborhood = 'São Paulo/SP';
 
 update scrapped_assets set city = 'Iracemápolis' 
-where city = 'Iracemópolis'
+where city = 'Iracemópolis';
 
 update cities_coordinates set city_name = 'Santa Bárbara d''Oeste' 
-where city_name = 'Santa Bárbara d`Oeste'
+where city_name = 'Santa Bárbara d`Oeste';
 
 update scrapped_assets set city = 'Santa Bárbara d''Oeste' 
-where city = 'Santa Bárbara d’Oeste'
+where city = 'Santa Bárbara d’Oeste';
 
 update scrapped_assets set city = 'Embu' 
-where city = 'Embu das Artes'
+where city = 'Embu das Artes';
 
 update scrapped_assets set city = 'Brasília' 
-where city = 'Brasilia'
+where city = 'Brasilia';
 
 update scrapped_assets set city = 'Quaraí' 
-where city = 'Quarai'
+where city = 'Quarai';
 
 update scrapped_assets set city = 'Campos dos Goytacazes' 
-where city = 'Campos Goytacazes'
+where city = 'Campos Goytacazes';
 
 update scrapped_assets set city = 'Rio de Janeiro' 
-where city = 'RIO DE JANEIRO'
+where city = 'RIO DE JANEIRO';
 
 update scrapped_assets set city = 'Maringá' 
-where city = 'Maringa'
+where city = 'Maringa';
 
 update scrapped_assets set city = 'Parobé' 
-where city = 'Parobe'
+where city = 'Parobe';
 
 update scrapped_assets set city = 'Juiz de Fora' 
-where city = 'Juiz Fora'
+where city = 'Juiz Fora';
 
 update scrapped_assets set city = 'Niterói' 
-where city = 'Niteroi'
+where city = 'Niteroi';
 
 update scrapped_assets set city = 'Cascavel' 
-where city = 'Cascável'
+where city = 'Cascável';
 
 update scrapped_assets set city = 'Sabáudia' 
-where city = 'Saubádia'
+where city = 'Saubádia';
 
 update scrapped_assets set city = 'São Luís' 
-where city = 'São Luis'
+where city = 'São Luis';
 
 update scrapped_assets set city = 'Carapicuíba' 
-where city = 'Carapicuiba'
+where city = 'Carapicuiba';
 
 update scrapped_assets set city = 'São Bernardo do Campo' 
-where city = 'São Bernando do Campo'
+where city = 'São Bernando do Campo';
 
 update scrapped_assets set city = 'São José do Xingu' 
-where city = 'São Jose do Xingú'
+where city = 'São Jose do Xingú';
 
 update scrapped_assets set city = 'Peruíbe' 
-where city = 'Peruibe'
+where city = 'Peruibe';
 
 update scrapped_assets set city = 'Duque de Caxias' 
-where city = 'Duque de Caixas'
+where city = 'Duque de Caixas';
 
 update scrapped_assets set city = 'Manaus' 
-where city = 'Manuas'
+where city = 'Manuas';
 
 update scrapped_assets set city = 'Igarassu' 
-where city = 'Pernambuco' and neighborhood = 'Igarassu'
+where city = 'Pernambuco' and neighborhood = 'Igarassu';
+
+update scrapped_assets set city = 'São Carlos', state = 'SP'
+where title = 'Atacadão São Carlos';
+
+update scrapped_assets set city = 'Cambé', state = 'PR'
+where title = 'Atacadão Londrina Cambé';
+
+update scrapped_assets set city = 'Uberaba', state = 'MG'
+where title = 'Mineirão Uberaba';
+
+update scrapped_assets set city = 'Natal', state = 'RN'
+where title = 'Mineirão Natal';
+
+update scrapped_assets set city = 'Juiz de fora', state = 'MG'
+where title = 'Mineirão Juiz de fora';
+
+update scrapped_assets set city = 'João Pessoa', state = 'PB'
+where title = 'Mineirão João Pessoa';
+
+update scrapped_assets set city = 'Uberlândia', state = 'MG'
+where title = 'Mineirão Uberlândia';
+
+update scrapped_assets set city = 'Foz do Iguaçu', state = 'PR'
+where title = 'Mineirão Foz do Iguaçu';
+
+update scrapped_assets set city = 'Contagem', state = 'MG'
+where title = 'Mineirão Contagem';
+
+update scrapped_assets set city = 'Barretos', state = 'SP'
+where title = 'Pernambucanas Barretos';
+
+update scrapped_assets set city = 'Guaíra', state = 'PR'
+where title = 'Pernambucanas Guaíra';
+
+update scrapped_assets set city = 'Ituverava', state = 'SP'
+where title = 'Pernambucanas Ituverava';
+
+update scrapped_assets set city = 'Mogi Das Cruzes', state = 'SP'
+where title = 'Pernambucanas Mogi Das Cruzes';
+
+update scrapped_assets set city = 'Pindamonhangaba', state = 'SP'
+where title = 'Pernambucanas Pindamonhangaba';
+
+update scrapped_assets set city = 'Taubaté', state = 'SP'
+where title = 'Pernambucanas Taubaté';
+
+update scrapped_assets set city = 'Barra Bonita', state = 'SP'
+where title = 'Pernambucanas Barra Bonita';
+
+update scrapped_assets set city = 'Itanhaém', state = 'SP'
+where title = 'Pernambucanas Itanhaém';
+
+update scrapped_assets set city = 'São Paulo', state = 'SP'
+where title = 'Pernambucanas Lapa';
+
+update scrapped_assets set city = 'Osasco', state = 'SP'
+where title = 'Pernambucanas Osasco';
+
+update scrapped_assets set city = 'Presidente Epitácio', state = 'SP'
+where title = 'Pernambucanas Presidente Epitácio';
+
+update scrapped_assets set city = 'Bandeirantes', state = 'PR'
+where title = 'Pernambucanas Bandeirantes';
+
+update scrapped_assets set city = 'Goioerê', state = 'PR'
+where title = 'Pernambucanas Goioerê';
+
+update scrapped_assets set city = 'Ivaiporã', state = 'PR'
+where title = 'Pernambucanas Ivaiporã';
+
+update scrapped_assets set city = 'Monte Alto', state = 'SP'
+where title = 'Pernambucanas Monte Alto';
+
+update scrapped_assets set city = 'Piracicaba', state = 'SP'
+where title = 'Pernambucanas Piracicaba';
+
+update scrapped_assets set city = 'Telêmaco Borba', state = 'PR'
+where title = 'Pernambucanas Telêmaco Borba';
+
+update scrapped_assets set city = 'Belo Horizonte', state = 'MG'
+where title = 'Pernambucanas Bh - Centro';
+
+update scrapped_assets set city = 'Ibiporã', state = 'PR'
+where title = 'Pernambucanas Ibiporã';
+
+update scrapped_assets set city = 'Jundiaí', state = 'SP'
+where title = 'Pernambucanas Jundiaí';
+
+update scrapped_assets set city = 'Olímpia', state = 'SP'
+where title = 'Pernambucanas Olímpia';
+
+update scrapped_assets set city = 'Poços De Caldas', state = 'MG'
+where title = 'Pernambucanas Poços De Caldas';
+
+update scrapped_assets set city = 'Votuporanga', state = 'SP'
+where title = 'Pernambucanas Votuporanga';
+
+update scrapped_assets set city = 'Araguari', state = 'MG'
+where title = 'Pernambucanas Araguari';
+
+update scrapped_assets set city = 'Itapecerica Da Serra', state = 'SP'
+where title = 'Pernambucanas Itapecerica Da Serra';
+
+update scrapped_assets set city = 'Lins', state = 'SP'
+where title = 'Pernambucanas Lins';
+
+update scrapped_assets set city = 'Ourinhos', state = 'SP'
+where title = 'Pernambucanas Ourinhos';
+
+update scrapped_assets set city = 'Ribeirão Preto', state = 'SP'
+where title = 'Pernambucanas Ribeirão Preto';
+
+update scrapped_assets set city = 'Assis Chateaubriand', state = 'PR'
+where title = 'Pernambucanas Assis Chateaubriand';
+
+update scrapped_assets set city = 'Cruzeiro', state = 'SP'
+where title = 'Pernambucanas Cruzeiro';
+
+update scrapped_assets set city = 'Itararé', state = 'SP'
+where title = 'Pernambucanas Itararé';
+
+update scrapped_assets set city = 'Mauá', state = 'SP'
+where title = 'Pernambucanas Mauá';
+
+update scrapped_assets set city = 'Pato Branco', state = 'PR'
+where title = 'Pernambucanas Pato Branco';
+
+update scrapped_assets set city = 'Taquarituba', state = 'SP'
+where title = 'Pernambucanas Taquarituba';
+
+update scrapped_assets set city = 'Videira', state = 'SC'
+where title = 'Pernambucanas Videira';
+
+update scrapped_assets set city = 'Apiaí', state = 'SP'
+where title = 'Pernambucanas Apiaí';
+
+update scrapped_assets set city = 'Dracena', state = 'SP'
+where title = 'Pernambucanas Dracena';
+
+update scrapped_assets set city = 'Itú', state = 'SP'
+where title = 'Pernambucanas Itú';
+
+update scrapped_assets set city = 'Mirandópolis', state = 'SP'
+where title = 'Pernambucanas Mirandópolis';
+
+update scrapped_assets set city = 'Pereira Barreto', state = 'SP'
+where title = 'Pernambucanas Pereira Barreto';
+
+update scrapped_assets set city = 'Tatuí', state = 'SP'
+where title = 'Pernambucanas Tatuí';
+
+update scrapped_assets set city = 'Alfenas', state = 'SP'
+where title = 'Pernambucanas Alfenas';
+
+update scrapped_assets set city = 'Capão Bonito', state = 'SP'
+where title = 'Pernambucanas Capão Bonito';
+
+update scrapped_assets set city = 'Itapetininga', state = 'SP'
+where title = 'Pernambucanas Itapetininga';
+
+update scrapped_assets set city = 'Machado', state = 'MG'
+where title = 'Pernambucanas Machado';
+
+update scrapped_assets set city = 'Palmas', state = 'TO'
+where title = 'Pernambucanas Palmas';
+
+update scrapped_assets set city = 'São José Do Rio Pardo', state = 'SP'
+where title = 'Pernambucanas São José Do Rio Pardo';
+
+update scrapped_assets set city = 'Amparo', state = 'SP'
+where title = 'Pernambucanas Amparo';
+
+update scrapped_assets set city = 'Piraju', state = 'SP'
+where title = 'Pernambucanas Carlos De Campos';
+
+update scrapped_assets set city = 'Itapira', state = 'SP'
+where title = 'Pernambucanas Itapira';
+
+update scrapped_assets set city = 'Marília', state = 'SP'
+where title = 'Pernambucanas Marília';
+
+update scrapped_assets set city = 'Passos', state = 'MG'
+where title = 'Pernambucanas Passos';
+
+update scrapped_assets set city = 'Sete Quedas', state = 'MS'
+where title = 'Pernambucanas Sete Quedas';
+
+update scrapped_assets set city = 'Adamantina', state = 'SP'
+where title = 'Pernambucanas Adamantina';
+
+update scrapped_assets set city = 'Campo Largo', state = 'PR'
+where title = 'Pernambucanas Campo Largo';
+
+update scrapped_assets set city = 'Carapicuíba', state = 'SP'
+where title = 'Pernambucanas Carapicuíba';
+
+update scrapped_assets set city = 'Itapeva', state = 'SP'
+where title = 'Pernambucanas Itapeva';
+
+update scrapped_assets set city = 'Marechal Cândido Rondon', state = 'PR'
+where title = 'Pernambucanas Marechal Cândido Rondon';
+
+update scrapped_assets set city = 'Paranaguá', state = 'PR'
+where title = 'Pernambucanas Paranaguá';
+
+update scrapped_assets set city = 'Sete De Setembro', state = 'RS'
+where title = 'Pernambucanas Sete De Setembro';
