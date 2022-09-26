393d4d355c42e1eee848ecfb11dd957503087530
Plinioc <plinio.lsc@gmail.com>
2022-09-25 23:54:10 -0300

    Parse full gdp data with more detailed data

--- scripts/loadCitiesGdp.js
+++ scripts/loadCitiesGdp.js
@@ -11,17 +11,36 @@ const parseBrlCurrency = (currency) => {
 
 const schemaToData = (cityCsvRow) => {
   return {
-    year: parseInt(cityCsvRow.ano),
-    gdpThousandsBrl: parseFloat(cityCsvRow.pib_bruto_1000brl),
-    gdpPerCapitaBrl: parseBrlCurrency(cityCsvRow.pib_per_capita_1brl),
-    state: cityCsvRow.sigla_uf,
-    code: cityCsvRow.codigo_municipio,
-    name: cityCsvRow.nome_municipio
+    year: parseInt(cityCsvRow.Ano),
+    greaterRegionCode: parseInt(cityCsvRow['Código da Grande Região']),
+    greaterRegionName: cityCsvRow['Nome da Grande Região'],
+    stateId: parseInt(cityCsvRow['Código da Unidade da Federação']),
+    stateAcronym: cityCsvRow['Sigla da Unidade da Federação'],
+    stateName: cityCsvRow['Nome da Unidade da Federação'],
+    cidyId: parseInt(cityCsvRow['Código do Município']),
+    cityName: cityCsvRow['Nome do Município'],
+    metropolitanRegion: cityCsvRow['Região Metropolitana'],
+    mesoRegionCode: parseInt(cityCsvRow['Código da Mesorregião']),
+    mesoRegionName: cityCsvRow['Nome da Mesorregião'],
+    isLegalAmazon: cityCsvRow['Amazônia Legal'],
+    isSemiArid: cityCsvRow['Semiárido'],
+    isSaoPauloRegion: cityCsvRow['Cidade-Região de São Paulo'],
+    agroValue1000Brl: parseBrlCurrency(cityCsvRow['Valor adicionado bruto da Agropecuária, \na preços correntes\n(R$ 1.000)']),
+    industryValue1000Brl: parseBrlCurrency(cityCsvRow['Valor adicionado bruto da Indústria,\na preços correntes\n(R$ 1.000)']),
+    servicesValue1000Brl: parseBrlCurrency(cityCsvRow['Valor adicionado bruto dos Serviços,\na preços correntes \n- exceto Administração, defesa, educação e saúde públicas e seguridade social\n(R$ 1.000)']),
+    publicExpendingValue1000Brl: parseBrlCurrency(cityCsvRow['Valor adicionado bruto da Administração, defesa, educação e saúde públicas e seguridade social, \na preços correntes\n(R$ 1.000)']),
+    totalValue1000Brl: parseBrlCurrency(cityCsvRow['Valor adicionado bruto total, \na preços correntes\n(R$ 1.000)']),
+    taxesValue1000Brl: parseBrlCurrency(cityCsvRow['Impostos, líquidos de subsídios, sobre produtos, \na preços correntes\n(R$ 1.000)']),
+    totalGdp1000Brl: parseBrlCurrency(cityCsvRow['Produto Interno Bruto, \na preços correntes\n(R$ 1.000)']),
+    gdpPerCapitaBrl: parseBrlCurrency(cityCsvRow['Produto Interno Bruto per capita, \na preços correntes\n(R$ 1,00)']),
+    mostValueableSector: cityCsvRow['Atividade com maior valor adicionado bruto'],
+    secondMostValueableSector: cityCsvRow['Atividade com segundo maior valor adicionado bruto'],
+    thirdMostValueableSector: cityCsvRow['Atividade com terceiro maior valor adicionado bruto'],
   }
 }
 
 const fetchData = async () => {
-  const csvUrl = 'https://gist.githubusercontent.com/pliniocampinas/3a0b327dbe7180dd984352e1be4b86e9/raw/4c89ca9aa4d46fbb4630e892613492e8593879b0/pib-municipios-2010-2019.csv'
+  const csvUrl = 'https://gist.githubusercontent.com/pliniocampinas/294f89397c51d53e8d9cbcc08194938e/raw/de296ef0e50fc37fe95350011906a23e59e6e233/PIB-dos-Municipios-2010-2019.csv'
   const { data } = await axios.get(csvUrl)
   const records = parse(data, {
     columns: true,
