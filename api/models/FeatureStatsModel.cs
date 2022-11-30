6e049ce4a4d243cbc65a3059575123617aaef3b4
Plinioc <plinio.lsc@gmail.com>
2022-11-30 00:24:37 -0300

    Compute complete national average stats

--- api/models/FeatureStatsModel.cs
+++ api/models/FeatureStatsModel.cs
@@ -3,9 +3,11 @@ namespace ScrapFunds.Models
   class FeatureStatsModel
   {
     public string FeatureName { get; set; } = string.Empty;
-    public decimal NationalAverage { get; set; }
+    public decimal NationalGdpPerCapitaBrlAverage { get; set; }
     public decimal FeatureGdpPerCapitaBrlAverage { get; set; }
-    public decimal FeatureTotalGdp1000BrlGrowthPercentAverage { get; set; }
+    public decimal NationalTotalGdpBrlGrowthPercentAverage { get; set; }
+    public decimal FeatureTotalGdpBrlGrowthPercentAverage { get; set; }
+    public decimal NationalPopulationGrowthPercentAverage { get; set; }
     public decimal FeaturePopulationGrowthPercentAverage { get; set; }
   }
 }
--- api/utilities/FeatureStatsHelper.cs
+++ api/utilities/FeatureStatsHelper.cs
@@ -7,22 +7,25 @@ namespace ScrapFunds.Helpers
   {
     public static FeatureStatsModel ExtractFeatureStats(List<FullCityVisualizationModel> results, string feature)
     {
-      var nationalAverage = results.Select(city => city.GdpPerCapitaBrl).Average();
-      var citiesWithSelecterFeature = results.Where(city => FeatureStatsHelper.FilterCityByFeature(city, feature));
-      var featureGdpPerCapitaBrlAverage = citiesWithSelecterFeature.Select(city => city.GdpPerCapitaBrl).Average();
-      var featureTotalGdp1000BrlGrowthPercentAverage = citiesWithSelecterFeature.Select(city => city.TotalGdp1000BrlGrowthPercent).Average() * 100;
-      var featurePopulationGrowthPercentAverage = citiesWithSelecterFeature.Select(city => city.PopulationGrowthPercent).Average() * 100;
+      var nationalGdpPerCapitaBrlAverage = results.Select(city => city.GdpPerCapitaBrl).Average();
+      var nationalTotalGdpBrlGrowthPercentAverage = results.Select(city => city.TotalGdp1000BrlGrowthPercent).Average();
+      var nationalPopulationGrowthPercentAverage = results.Select(city => city.PopulationGrowthPercent).Average();
+      var citiesWithSelectedFeature = results.Where(city => FeatureStatsHelper.FilterCityByFeature(city, feature));
+      var featureGdpPerCapitaBrlAverage = citiesWithSelectedFeature.Select(city => city.GdpPerCapitaBrl).Average();
+      var featureTotalGdp1000BrlGrowthPercentAverage = citiesWithSelectedFeature.Select(city => city.TotalGdp1000BrlGrowthPercent).Average() * 100;
+      var featurePopulationGrowthPercentAverage = citiesWithSelectedFeature.Select(city => city.PopulationGrowthPercent).Average() * 100;
 
       return new FeatureStatsModel()
       {
         FeatureName = feature,
-        NationalAverage = Decimal.Round(nationalAverage, 2),
+        NationalGdpPerCapitaBrlAverage = Decimal.Round(nationalGdpPerCapitaBrlAverage, 2),
+        NationalTotalGdpBrlGrowthPercentAverage = Decimal.Round(nationalTotalGdpBrlGrowthPercentAverage, 2),
+        NationalPopulationGrowthPercentAverage = Decimal.Round(nationalPopulationGrowthPercentAverage, 2),
         FeatureGdpPerCapitaBrlAverage = Decimal.Round(featureGdpPerCapitaBrlAverage, 2),
-        FeatureTotalGdp1000BrlGrowthPercentAverage = Decimal.Round(featureTotalGdp1000BrlGrowthPercentAverage, 2),
+        FeatureTotalGdpBrlGrowthPercentAverage = Decimal.Round(featureTotalGdp1000BrlGrowthPercentAverage, 2),
         FeaturePopulationGrowthPercentAverage = Decimal.Round(featurePopulationGrowthPercentAverage, 2),
       };
     }
-    
 
     private static bool FilterCityByFeature(FullCityVisualizationModel city, string feature)
     {
