8be2c1159d01b29d6f237cf05048db89b810b2b8
Plinioc <plinio.lsc@gmail.com>
2022-12-15 23:37:27 -0300

    Start to model funds and assets queries

---
+++ api/models/UniqueFundModel.cs
@@ -0,0 +1,9 @@
+namespace ScrapFunds.Models
+{
+  class UniqueFundModel
+  {
+    public string Acronym { get; set; } = string.Empty;
+    public string LongName { get; set; } = string.Empty;
+    public string Admin { get; set; } = string.Empty;
+  }
+}
